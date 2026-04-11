d860dd1493d9c6182b3cfa37ff1af18abf88c472
mani1604 <manish_sharma1604@yahoo.com>
2026-04-11 19:41:43 +0530

    sqlite to postgres migration

--- backend/drf_project/drf_project/settings.py
+++ backend/drf_project/drf_project/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'drf_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'lms_db',
+        'USER': 'lms_user',
+        'PASSWORD': 'Test@123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
