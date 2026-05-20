f082d73af47c33f40905e337890d2515074c1d83
mani1604 <manish_sharma1604@yahoo.com>
2026-05-20 10:14:46 +0530

    Changes for render deployment via dockerhub

--- backend/drf_project/drf_project/settings.py
+++ backend/drf_project/drf_project/settings.py
@@ -29,6 +29,7 @@ SECRET_KEY = config("SECRET_KEY") #  Load .env
 DEBUG = config("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = [
+    "localhost:8000",
     ".onrender.com", # app1.onrender.com
     config("RENDER_EXTERNAL_HOSTNAME", default="")
 ]
